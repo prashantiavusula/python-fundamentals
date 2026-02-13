3d4c9478dd69a2c2ba2f3c88805e81d9a52e9377
prashanti avusula <avusulaprashanti@gmail.com>
2026-02-13 21:29:32 +0530

    Create 01_introduction.py

---
+++ 01_introduction.py
@@ -0,0 +1,46 @@
+WHAT IS PYTHON?
+Python is a programming language that is easy to read and write. You can use it to make websites, apps, games, or even teach computers to learn things (AI).
+Made by: Guido van Rossum in 1991
+______________________________________________________________________________________________________________________________________________________________________________________________________________________
+WHY PYTHON IS GOOD ?
+Easy to learn
+Code is short and clear
+Works on any computer
+Lots of free tools and libraries
+Case-sensitive – Name and name are different
+save the file(.py) extension
+the name of the python chossen from monty python flying cirvus bcc show
+it has large community
+large standard lib/pac/module/frameworks
+_____________________________________________________________________________________________________________________________________________________________________________________________________________________
+Python is an INTERPRETED language
+INTERPRETED VS COMPLIED
+Compiled languages (like C or C++) are transformed into machine code before running. Once compiled, the program runs directly on the computer’s hardware.
+Interpreted languages (like Python, Ruby, or JavaScript) are executed by an interpreter at runtime, which reads your code and runs it line by line.
+
+ADVANTGES OF INTERPRETED LANGUAGE
+Easier to test and debug (you can run code snippets interactively).
+More portable across different systems (the same Python code can run anywhere Python is installed).
+
+DISADVANTGE
+Generally slower than compiled languages, because the interpreter has to process the code on the fly.
+
+______________________________________________________________________________________________________________________________________________________________________________________________________________________
+WHRERE SHOULD I USE PYTHON?
+Web Development** – websites and apps (Django, Flask)
+Data Science & Analytics** – analyze data, make charts (Pandas, NumPy)
+AI & Machine Learning** – smart apps and predictions (TensorFlow, PyTorch)
+Automation** – automate tasks and scripts
+Game Development** – make simple games (Pygame)
+Software Development** – desktop apps (Tkinter, PyQt)
+Learning & Education** – great for beginners
+_____________________________________________________________________________________________________________________________________________________________________________________________________________________
+HOW SHOULD I USE PYTHON?
+Install Python – get it from python.org.
+Open a place to write code – IDLE, VS Code, or online (like Replit).
+Write your first program:
+print("Hello, World!")
+Run it – click Run or use python filename.py in terminal
+_____________________________________________________________________________________________________________________________________________________________________________________________________________________
+
+
