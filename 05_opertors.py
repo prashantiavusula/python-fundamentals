411d93b9ae090fb7998a6635dd0ea8ed049d32f6
prashanti avusula <avusulaprashanti@gmail.com>
2026-02-20 18:58:35 +0530

    Update 05_opertors.py

--- 05_opertors.py
+++ 05_opertors.py
@@ -1,1 +1,21 @@
+WHAT IS OPERATORS?
+operator is a special symbol or keyword that tells the computer to perform a specific operation on one or more values (called operands)
 
+Operands → the values you operate on
+Operator → the symbol that defines the operation
+Result → output of the operation
+
+a = 5
+b = 3
+c = a + b   # '+' is the operator, a and b are operands
+print(c)    # Output: 8
+_______________________________________________________________________________________________________________________________________________________________________________________________________________
+TYPES OF OPERATORS IN PYTHON?
+Arithmetic operators → +, -, *, /, %, **, //
+Comparison operators → ==, !=, >, <, >=, <=
+Logical operators → and, or, not
+Assignment operators → =, +=, -=, etc.
+Bitwise operators → &, |, ^, ~, <<, >>
+Identity operators → is, is not
+Membership operators → in, not in
+______________________________________________________________________________________________________________________________________________________________________________________________________
