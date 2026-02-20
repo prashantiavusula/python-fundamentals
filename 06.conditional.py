ac415ef6674b7b61ff22aee0ef707bc67745da46
prashanti avusula <avusulaprashanti@gmail.com>
2026-02-20 20:56:21 +0530

    Update 06.conditional.py

--- 06.conditional.py
+++ 06.conditional.py
@@ -12,14 +12,18 @@ if statement is a condtional statemnet that excutes a block of code wher condtio
  SYNTAX
 if condtion:
   #code 
-
+____________________________________________________________________________________________________________________________________________________________________________________
  else statement
   if the statment is condtiona statement  that excutes a block of code where if condtion is false
  SYNTAX
 if condtion:
   #code is true
-else condtion:
-  #code  is fsle
+else:
+  #code  is false
+if checks a condition — “Does this happen?” ✅
+elif checks another condition if the first was false — “What about this case?” ✅
+else runs when nothing else matched — it doesn’t need a condition, because if Python reached else, it already knows all previous conditions were false
+____________________________________________________________________________________________________________________________________________________________________________________________
 
 elif statement
  elif(short for else if) is used in a condtional statment to check whether  another condtion if the pervious if condtion is false
