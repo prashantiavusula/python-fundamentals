a5f2ccf5b105048e288134e0099a8a5edd76294c
prashanti avusula <avusulaprashanti@gmail.com>
2026-02-20 20:51:22 +0530

    Update 06.conditional.py

--- 06.conditional.py
+++ 06.conditional.py
@@ -1,1 +1,33 @@
+WHAT IS CONDTIONAL STATEMENT?
+condintaional statment is  a stament that excutes a block of code based on condtion
 
+THR CONDTIONAL STAMENT ARE 
+ if
+else
+elif
+__________________________________________________________________________________________________________________________________________________________________________________________________________  
+  IF STATEMENT
+if statement is a condtional statemnet that excutes a block of code wher condtion is true
+
+ SYNTAX
+if condtion:
+  #code 
+
+ else statement
+  if the statment is condtiona statement  that excutes a block of code where if condtion is false
+ SYNTAX
+if condtion:
+  #code is true
+else condtion:
+  #code  is fsle
+
+elif statement
+ elif(short for else if) is used in a condtional statment to check whether  another condtion if the pervious if condtion is false
+SYNTAX
+if condtion:
+  #code
+elif condtion:
+    #code
+else:
+  #code
+  
