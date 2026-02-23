68b6c1f2960fcc77915a374cf36a1fa122e96835
prashanti avusula <avusulaprashanti@gmail.com>
2026-02-23 21:49:09 +0530

    Create 07_loop.py

---
+++ 07_loop.py
@@ -0,0 +1,27 @@
+What Is a Loop in Python?
+A loop is a way to repeat a block of code multiple times without writing it again and again.
+Instead of doing this:
+
+Why Do We Use Loops?
+Loops are useful when:
+You want to repeat something many times
+You want to go through items in a list
+You want to run code while a condition is true
+
+types of Loops in Python
+1️⃣ for Loop
+Used when you want to repeat something a specific number of times or go through a group of items.
+Example idea:
+“Do this 5 times”
+“Print every item in a list”
+It runs once for each item.
+
+2️⃣ while Loop
+Used when you want to repeat something as long as a condition is true.
+Example idea:
+“Keep running until the number becomes 10”
+“Keep asking until the user gives the correct answer”
+It stops when the condition becomes false.
+
+for loop → Used when you know how many times to repeat
+while loop → Used when you repeat based on a condition
