3fbcc5ce17a71204fd99b305d34b091460eb2698
prashanti avusula <avusulaprashanti@gmail.com>
2026-03-28 21:30:01 +0530

    Create function.py

---
+++ function.py
@@ -0,0 +1,14 @@
+what is function?
+A function definition is a block of code that creates (defines) a function, which you can reuse whenever needed.
+
+Function name should be valid
+Use letters, numbers, underscore _
+Do NOT start with number
+No spaces
+
+def function_name(parameters):
+    # body of the function
+    statements
+    return value
+#return is optinal
+#parameters inside the paranthsis
