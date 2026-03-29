301f9531f4b2daabf7da63d0465f30c8dcf2ae95
prashanti avusula <avusulaprashanti@gmail.com>
2026-03-29 16:30:47 +0530

    Create operator.py

---
+++ operator.py
@@ -0,0 +1,38 @@
+1. Arithmetic Operators (Math operations)
+| Operator | Meaning             | Example       |
+| -------- | ------------------- | ------------- |
+| `+`      | Addition            | `5 + 2 = 7`   |
+| `-`      | Subtraction         | `5 - 2 = 3`   |
+| `*`      | Multiplication      | `5 * 2 = 10`  |
+| `/`      | Division            | `5 / 2 = 2.5` |
+| `//`     | Floor Division      | `5 // 2 = 2`  |
+| `%`      | Modulus (remainder) | `5 % 2 = 1`   |
+| `**`     | Power               | `5 ** 2 = 25` |
+
+2. Comparison Operators (Check True/False)
+| Operator | Meaning          | Example          |
+| -------- | ---------------- | ---------------- |
+| `==`     | Equal to         | `5 == 5 → True`  |
+| `!=`     | Not equal        | `5 != 2 → True`  |
+| `>`      | Greater than     | `5 > 2 → True`   |
+| `<`      | Less than        | `5 < 2 → False`  |
+| `>=`     | Greater or equal | `5 >= 5 → True`  |
+| `<=`     | Less or equal    | `5 <= 2 → False` |
+  
+3. Logical Operators (Combine conditions)
+| Operator | Meaning           | Example                  |
+| -------- | ----------------- | ------------------------ |
+| `and`    | Both true         | `True and False → False` |
+| `or`     | At least one true | `True or False → True`   |
+| `not`    | Reverse result    | `not True → False`       |
+
+4. Assignment Operators
+| Operator | Example                |
+| -------- | ---------------------- |
+| `=`      | `x = 5`                |
+| `+=`     | `x += 2` → `x = x + 2` |
+| `-=`     | `x -= 2`               |
+| `*=`     | `x *= 2`               |
+| `/=`     | `x /= 2`               |
+
+
